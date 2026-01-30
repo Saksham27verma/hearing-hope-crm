bd7f3673337faec99bfface7be87e281091ac949
Saksham Verma <saksham27verma@gmail.com>
2026-01-30 16:42:40 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicon (place your file at: public/favicon.png) */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </head>
       <body>
         <ThemeRegistry>
           <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
